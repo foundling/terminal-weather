dfc5ce376e02b658d60bc0aac6a009c4997378cb
alex <alexramsdell@gmail.com>
2021-08-18 19:55:12 -0400

    clean up.

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,14 @@ import { promisify }  from 'util';
 import emojiMap, { EmojiMap, FormatData } from './emojis';
 
 const readFilePromise = promisify(readFile)
-const CONFIG_PATH = path.join(homedir(), '.tw-config.json');
+const CONFIG_PATH = path.join(homedir(), '.twconfig'); // TODO: make this a command-line flag
 
 type WeatherQueryOptions = {
-  units: 'standard' | 'metric' | 'imperial';
-  exclude: string;
   appid: string;
+  lat: number;
+  lon: number;
+  exclude: string;
+  units: 'standard' | 'metric' | 'imperial';
 }
 
 type WeatherDescription = {
@@ -20,21 +22,15 @@ type WeatherDescription = {
   description: string;
 };
 
-type TempResponse = {
+type TemperatureData = {
   min: number;
   max: number;
 };
 
 type DailyWeatherResponse = {
   dt: Date;
-  temp: TempResponse;
+  temp: TemperatureData;
   weather: WeatherDescription[];
-};
-
-type WeatherResponse = {
-  lat: number;
-  lon: number;
-  daily: DailyWeatherResponse[];
 };
 
 type Coordinates = {
@@ -42,21 +38,44 @@ type Coordinates = {
   lon: number;
 }
 
+type WeatherResponse = {
+  coords: Coordinates;
+  lon: number;
+  daily: DailyWeatherResponse[];
+};
+
 async function readConfig() {
+
   try {
-    const json = await readFilePromise(CONFIG_PATH, 'utf-8');
-    return JSON.parse(json);
+
+    const serialized = await readFilePromise(CONFIG_PATH, 'utf-8');
+    const lines = serialized.split('\n').filter(Boolean);
+    const config = lines.reduce((config, line:string) => {
+      const [name, value] = line.split('=').map(nameOrValue => nameOrValue.trim())
+      config.set(name, value);
+      return config;
+   }, new Map());
+
+   return config;
+
   } catch(e) {
+
     console.error('Error: could not read your config file with your API KEY.')
-    // TODO: run a cli --help command here 
+    console.error(e);
+
+    // TODO: run a cli --help command here
+
     process.exit(1);
+
   }
+
 }
 
 async function getLocationFromIpAddress():Promise<Coordinates> {
 
     const result:Response = await fetch('http://ip-api.com/json');
-    const { lat, lon } = await result.json(); 
+    console.log(result.status)
+    const { lat, lon } = await result.json();
     return { lat, lon }
 
 }
@@ -73,13 +92,12 @@ function buildSearchParamString(options: Coordinates & WeatherQueryOptions):stri
 
 }
 
-async function getWeatherFromCoords(coordinates: Coordinates, options: WeatherQueryOptions):Promise<WeatherResponse> {
+async function getWeatherFromCoords(queryParams: WeatherQueryOptions):Promise<WeatherResponse> {
 
-  const { lat, lon } = coordinates;
-
-  const qp = buildSearchParamString({ ...coordinates, ...options });
+  const qp = buildSearchParamString(queryParams);
   const owmEndpoint = `https://api.openweathermap.org/data/2.5/onecall?${qp}`
   const response:Response = await fetch(owmEndpoint);
+  console.log(response.status)
   const weather:WeatherResponse = await response.json();
 
   return weather
@@ -92,7 +110,7 @@ function formatWeatherData(data: DailyWeatherResponse) {
 
   const { min, max } = data.temp;
   const [ hi, lo ] = [ Math.round(max), Math.round(min) ];
-  const { description, main } = data.weather[0];  
+  const { description, main } = data.weather[0];
   const weather = emojiMap[main].icon ? emojiMap[main].icon : emojiMap[main].text;
   result += `${weather}  ${hi}/${lo} |`;
 
@@ -102,25 +120,21 @@ function formatWeatherData(data: DailyWeatherResponse) {
 
 async function main() {
 
-  const { API_KEY, temp_unit } = await readConfig(); 
-  const weatherQueryOptions = {
+  const config = await readConfig();
+  console.log(config)
+  const temp_unit = config.get('temp_unit');
+  const API_KEY = config.get('API_KEY');
+  const { lat, lon }:Coordinates = await getLocationFromIpAddress();
+
+  const queryParams = {
     units: temp_unit || 'imperial',
     exclude: 'minutely,hourly',
     appid: API_KEY,
+    lat,
+    lon,
   };
 
-  const coords:Coordinates = await getLocationFromIpAddress();  
-  const { daily } = await getWeatherFromCoords(coords, weatherQueryOptions);
-
-  const formatOptions = {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-  };
-
-  let result = '';
-
+  const { daily } = await getWeatherFromCoords(queryParams);
   return daily.map(formatWeatherData).join(' ');
 
 }
