dbf3e96d8879eee2a0280772bc785b3d66c849b7
Alex Ramsdell <aramsdell@artandlogic.com>
2023-03-13 21:34:23 -0400

    remove separate version.json file and associated postinstall script.

--- scripts/postinstall.js
+++
@@ -1,5 +0,0 @@
-// write version to file in src, so we can use rootDir in tsconfig when building.
-
-const { writeFileSync } = require('fs');
-const version = require('../package.json').version;
-writeFileSync('./src/version.json', `{ "version": "${version}" }`);
